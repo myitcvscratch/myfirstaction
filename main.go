4987c72b67289f4e696d56f3425b4af24ac185c2
Paul Jolly <paul@myitcv.io>
2020-02-03 21:44:20 +0000

    Try an exec

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
 
-	"github.com/sethvargo/go-githubactions"
+	gh "github.com/sethvargo/go-githubactions"
 )
 
 func main() {
-	fruit := githubactions.GetInput("fruit")
+	td, err := ioutil.TempDir("", "")
+	if err != nil {
+		gh.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	cmd := exec.Command("git", "clone", "https://github.com/vim/vim", td)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		gh.Fatalf("Failed to run %v: %v", strings.Join(cmd.Args, " "), err)
+	}
+	fruit := gh.GetInput("fruit")
 	fmt.Printf("You gave me the fruit: %v; thank you!\n", fruit)
 }
