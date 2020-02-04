86362b6f11cb7fbe0ac78394690017ef7025643e
Paul Jolly <paul@myitcv.io>
2020-02-04 09:57:06 +0000

    Switch to running go

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 "use strict";
 var spawn = require('child_process').spawn;
-spawn('go', ['run', 'main.go'], { stdio: 'inherit' });
+const go = spawn('go', ['run', 'main.go'], { stdio: 'inherit' });
+go.on('close', (code) => {
+	process.exit(code);
+});
