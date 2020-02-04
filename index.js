10e2cc0eb7dc9c314ab9f6baab2dc2445eef805b
Paul Jolly <paul@myitcv.io>
2020-02-04 10:26:27 +0000

    Latest changes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 "use strict";
-const path = require('path');
-var spawn = require('child_process').spawn;
-console.log(path.join(__dirname, 'main.go'))
-const go = spawn('go', ['version'], { stdio: 'inherit' });
-go.on('close', (code) => {
-	process.exit(code);
-});
+
+const spawn = require('child_process').spawn;
+
+async function run() {
+	var args = Array.prototype.slice.call(arguments);
+	const cmd = spawn(args[0], args.slice(1), { stdio: 'inherit' });
+	const exitCode = await new Promise( (resolve, reject) => {
+			cmd.on('close', resolve);
+	});
+	if (exitCode != 0) {
+		process.exit(exitCode);
+	}
+}
+
+(async function() {
+	await run('git', 'log', '-1');
+	// await run('go', 'version');
+	// const path = require('path');
+	// await run('go', 'run', path.join(__dirname, 'main.go'))
+})()
