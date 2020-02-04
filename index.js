c160fe07367823dd12c42afd1612d9133d427489
Paul Jolly <paul@myitcv.io>
2020-02-04 10:36:39 +0000

    Latest changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn;
 
 async function run() {
 	var args = Array.prototype.slice.call(arguments);
-	const cmd = spawn(args[0], args.slice(1), { stdio: 'inherit' });
+	const cmd = spawn(args[0], args.slice(1), { stdio: 'inherit', cwd: __dirname });
 	const exitCode = await new Promise( (resolve, reject) => {
 			cmd.on('close', resolve);
 	});
