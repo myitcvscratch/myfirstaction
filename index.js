e98c788ab4676701dba454ebdc8c7de812c84f50
Paul Jolly <paul@myitcv.io>
2020-02-04 10:39:36 +0000

    Latest changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log(">> Time is: ", (new Date(Date.now())).toLocaleString());
 console.log("");
 
 (async function() {
-	await run('git', 'log', '-1');
+	// await run('git', 'log', '-1');
 	// await run('go', 'version');
 	// const path = require('path');
 	// await run('go', 'run', path.join(__dirname, 'main.go'))
